5149c37ba937c32e134fe07e18cd1aaa2000ee2f
Gleb Mosko <gleb.yandex.ru>
2025-08-05 18:34:09 +0300

    Создание окна и отображение его

--- ExampleApp/SceneDelegate.swift
+++ ExampleApp/SceneDelegate.swift
@@ -13,10 +13,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+       
+        // наша сцена
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        // Создаем окно для сцены
+        let window = UIWindow(windowScene: windowScene)
+        // Устанавливаем корневой контроллер в окне
+        window.rootViewController = ViewController()
+        // отображаем оено на экране
+        window.makeKeyAndVisible()
+        // сохраняем ссылку на окно что бы оно не было выгружено
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
