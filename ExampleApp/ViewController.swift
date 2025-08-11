d7c3a6ea20bfe0468eda63a9505b26aafd0d4ee5
Gleb Mosko <gleb.yandex.ru>
2025-08-11 18:56:53 +0300

    add-func-setapLabel

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     
     private let helper = Helper()
+    private let textLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
         updateNumbers()
+        
+        setupLabel()
+        view.addSubview(textLabel)
     }
     
     private func updateNumbers() {
@@ -23,5 +27,13 @@ class ViewController: UIViewController {
         for number in helper.getNumbers() {
             print(number)
         }
+    }
+    
+    private func setupLabel() {
+        let firstNumber = helper.getNumbers().first
+        textLabel.text = firstNumber?.formatted()
+        textLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        textLabel.textColor = .red
+        textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
     }
 }
