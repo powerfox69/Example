ea288980bc40c409d5546981b827663b84bd0924
Gleb Mosko <gleb.yandex.ru>
2025-08-12 19:54:45 +0300

    add Image and container

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
     
     private let helper = Helper()
     private let textLabel = UILabel()
+    private let imageView = UIImageView()
+    private let imageContainerView = UIView()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +22,11 @@ class ViewController: UIViewController {
         updateNumbers()
         
         setupLabel()
+        setupImageView()
+        setupView()
+        
         view.addSubview(textLabel)
+        view.addSubview(imageContainerView)
     }
     
     private func updateNumbers() {
@@ -36,4 +44,30 @@ class ViewController: UIViewController {
         textLabel.textColor = .red
         textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
     }
+    
+    private func setupImageView() {
+        imageView.image = UIImage(named: "hog")
+        imageView.frame = CGRect(x: 30, y: 130, width: 100, height: 200)
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        
+        imageContainerView.frame = imageView.bounds
+        imageContainerView.layer.shadowColor = UIColor.black.cgColor
+        imageContainerView.layer.shadowOffset = CGSize(width: 15, height: 15)
+        imageContainerView.layer.shadowOpacity = 1
+        imageContainerView.layer.shadowRadius = 10
+        
+        imageContainerView.addSubview(imageView)
+    }
+    
+    private func setupView() {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        
+        view.layer.insertSublayer(gradient, at: 0)
+    }
 }
+
